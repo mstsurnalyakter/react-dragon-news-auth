9ea6d0735964382e6c7f993ab1fa9a3e546c7e9a
Programming Hero <programming.hero1@gmail.com>
2023-09-30 23:43:12 -0500

    folder structure and router setup

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+
+const Root = () => {
+    return (
+        <div>
+            <Outlet></Outlet>      
+        </div>
+    );
+};
+
+export default Root;
