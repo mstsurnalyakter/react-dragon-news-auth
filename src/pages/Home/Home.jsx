480fea2302e47d04b5a59d957368ef4f85cf5325
Programming Hero <programming.hero1@gmail.com>
2023-10-01 07:52:15 -0500

    home page layout and moment js installed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 
 const Home = () => {
@@ -7,7 +9,17 @@ const Home = () => {
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h2 className="text-3xl font-poppins font-bold">This is Home</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2 border">
+                    <h2 className="text-4xl">News coming soon...</h2>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
-
+import logo from '../../../assets/logo.png';
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h2 className="text-3xl">Header</h2>
+            <img src={logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
+            <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
