80ae22fb723d3c8c19b4d535ff68c37b78d282bb
Programming Hero <programming.hero1@gmail.com>
2023-10-01 19:47:12 -0500

    left nav load category and module summary

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
-        <div>
-            <h2 className="text-2xl">Left Side</h2>
+        <div className="space-y-6">
+            <h2 className="text-2xl">All Categories</h2>
+            {
+                categories.map(category => <Link 
+                    className="block ml-4 text-xl font-semibold" 
+                    key={category.id}
+                    to={`/category/${category.id}`}
+                    >{category.name}</Link>)
+            }
         </div>
     );
 };
