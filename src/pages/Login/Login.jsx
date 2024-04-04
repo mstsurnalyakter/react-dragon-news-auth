4c016eaf7b6604f3c9f30e40cc638fb6052bf9e4
mstsurnalyakter <surnalyms@gmail.com>
2024-04-05 04:57:16 +0600

    added login route

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../Shared/Navbar/Navbar'
+
+const Login = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+
+
+export default Login
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root></Root>, 
+        element: <Root></Root>,
         children: [
             {
                 path: '/',
                 element: <Home></Home>
-            }
+            },
+            {
+                path: '/login',
+                element: <Login/>
+            },
         ]
-    }    
+    }
 ]);
 
 export default router;
