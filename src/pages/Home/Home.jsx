7320e2ffccce8fcf5aac7fa2e0120f03f8839aa3
mstsurnalyakter <surnalyms@gmail.com>
2024-04-05 10:27:33 +0600

    News page added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,31 @@
+import { useLoaderData } from "react-router";
 import Header from "../Shared/Header/Header";
 import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 import BreakingNews from "./BreakingNews";
+import NewCard from "./NewCard";
 
 
 const Home = () => {
+    const news = useLoaderData();
+    console.log(news);
     return (
         <div>
             <Header></Header>
             <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-                <div className="border">
+                <div className="">
                     <LeftSideNav></LeftSideNav>
                 </div>
-                <div className="md:col-span-2 border">
-                    <h2 className="text-4xl">News coming soon...</h2>
+                <div className="md:col-span-2  space-y-6">
+                   {/* news container */}
+                   {
+                    news.map((singleNews)=><NewCard key={singleNews._id} singleNews={singleNews} />)
+                   }
                 </div>
-                <div className="border">
+                <div className="">
                     <RightSideNav></RightSideNav>
                 </div>
             </div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,26 +3,33 @@ import Root from "../layouts/Root";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import News from "../pages/News/News";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root></Root>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: '/login',
-                element: <Login/>
-            },
-            {
-                path: '/register',
-                element: <Register/>
-            },
-        ]
-    }
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch("/news.json"),
+      },
+      {
+        path: "/news/:id",
+        element: <News />,
+        loader: () => fetch("/news.json"),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
 ]);
 
 export default router;
